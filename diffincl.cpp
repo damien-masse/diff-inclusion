dadfd6b301d1fd4d70fccb168c249af2173bcacb
Damien Masse <damien.masse@univ-brest.fr>
2021-09-23 21:51:34 +0200

    Mise en forme, meilleure gestion des problèmes entre la boite approximée et la boite calculée.

--- diffincl.cpp
+++ diffincl.cpp
@@ -146,7 +146,9 @@ namespace diffincl {
   // very basic enclosing for evolution
   IntervalVector DiffInclusion::extend_box_basic(const IntervalVector& frame,
                  	const IntervalVector& Xbox,
-                 	const Interval& tim) {
+                 	const Interval& tim, 
+			double inflation_factor,
+			bool last_contraction) {
       double tstep = tim.diam();
       Interval btime(0.0,tstep);
       /* estimation des pentes */
@@ -156,10 +158,12 @@ namespace diffincl {
       /* compute approximation of the result */
       IntervalVector Res = Xbox + btime*k1;
       /* inflation */
-      Res.inflate(1.2,0.0);
+      Res.inflate(inflation_factor,0.0);
       Res &= frame;
-      Res = Xbox + btime * this->teval_vector(tim,Res);
-      Res &= frame;
+      if (last_contraction) {
+        Res = Xbox + btime * this->teval_vector(tim,Res);
+        Res &= frame;
+      }
       return Res;
   }
   
@@ -167,26 +171,26 @@ namespace diffincl {
         
 
   // compute next step, from linear approximation and uncertainty
-  // centzone : center of the zone
   // fcent = f(center of the zone, half time)
-  // tVect = mid of temporal differential
-  // jac : mid of jacobian
-  // prodExpM : cumulated product of exponential (without time)
+  // tVect = mid of temporal differential (dfun/dt (mid_t))
+  // jac : mid of jacobian (dfun/dx (zone))
+  // ExpM (out) : exponential (not cumulated)
+  // evolCenter (out) : evolution of the ``center'' 
+  // tauProdExpM (out) : intermediate exponential/[0,1] (!not! cumulated)
+  // tauCent (out) : evolution of the center / [0,1] (with uncertainty)
   // uncert : U value (uncertity on f)
   // double tsteps : time step
   // f(x,t) \in fcent + (t-tmid)*tvect + jac*(x-x0) + uncert
-  void DiffInclusion::compute_next_step(const Vector& centzone,
-			 const Vector& fcent,
+  void DiffInclusion::compute_next_step(const Vector& fcent,
                          const Vector& tVect,
                          const Matrix& jac,
                          const IntervalVector& uncert,
                          double tsteps,
-  	                 IntervalVector& Cent, 
-                         IntervalMatrix& prodExpM,
+  	                 IntervalVector& evolCenter, 
+                         IntervalMatrix& ExpM,
 			 IntervalVector& tauCent,
 			 IntervalMatrix& tauProdExpM) {
-     // global computation
-     IntervalMatrix ExpM(dim,dim);
+     // global computation of the exponentials and their integral
      IntervalMatrix IExpM(dim,dim);
      IntervalMatrix VExpM(dim,dim);
      IntervalMatrix tauExpM(dim,dim);
@@ -194,40 +198,108 @@ namespace diffincl {
      IntervalMatrix tauVExpM(dim,dim);
      Matrix IntAbs(dim,dim);
      const Matrix Id = Matrix::eye(dim);
+
      // computation of exponentials and other values
-     global_exp(jac,tsteps,true,time_dependent,ExpM,tauExpM,IExpM,tauIExpM,VExpM,tauVExpM,IntAbs);
-//     std::cout << "\n\nExpM" << ExpM << "\ntauExpM" << tauExpM << "\nIExpM" << IExpM << "\ntauIExpM" << tauIExpM << "\nVExpM" << VExpM << "\ntauVExpM" << tauVExpM << "\nIntAbs" << IntAbs << "\n";
-     // intermediates values
+     global_exp(jac,tsteps,true,time_dependent,
+		ExpM,tauExpM,IExpM,tauIExpM,
+		VExpM,tauVExpM,IntAbs);
+
+     if (debug_level>5)
+	std::cout << "\n\nExpM" << ExpM << "\ntauExpM" 
+		  << tauExpM << "\nIExpM" << IExpM << "\ntauIExpM" 
+		  << tauIExpM << "\nVExpM" << VExpM << "\ntauVExpM" 
+		  << tauVExpM << "\nIntAbs" << IntAbs << "\n";
+
+     ////// ``intermediates'' values  (between 0 and tsteps) 
+
+     // uncertainty
      Vector vuncert = IntAbs * uncert.ub();
-     IntervalVector evolCenter = tauIExpM * fcent;
+     // ``derivation'' of the center ( f(cent) * int exp(M\tau) )
+     tauCent = tauIExpM * fcent;
+     // non-autonomous factor
      if (time_dependent) {
-           evolCenter += tauVExpM * tVect;
+           tauCent += tauVExpM * tVect;
      }
-//     std::cout << "tauIExpM * fcent : " << evolCenter << "\n";
-     tauCent = evolCenter;
-     for (int i=0;i<dim;i=i+1) (tauCent[i]).inflate(vuncert[i]); // FIXME !!!
+     if (debug_level>5)
+        std::cout << "derivation without uncertainty : " << tauCent << "\n";
+     // adding the uncertainty to evolCenter
+     for (int i=0;i<dim;i=i+1) (tauCent[i]).inflate(vuncert[i]); 
+     // new product
      tauProdExpM = tauExpM;
 
-     // final values
+     ///// ``final'' values (at tsteps)
+     // ``derivation'' of the center ( f(cent) * int exp(M\tau) )
      evolCenter = IExpM * fcent;
+     // non-autonomous factor
      if (time_dependent) {
            evolCenter += VExpM * tVect;
      }
-     Cent = centzone + ExpM*(Cent-centzone) + evolCenter;
-     for (int i=0;i<dim;i=i+1) (Cent[i]).inflate(vuncert[i]);
-     prodExpM = ExpM * prodExpM;
-
+     for (int i=0;i<dim;i=i+1) (evolCenter[i]).inflate(vuncert[i]);
   }
+
+  ///// possible step 
+  void DiffInclusion::fwd_step(const IntervalVector& constraint,
+		const IntervalVector& Xbox,
+		const IntervalVector& approxbox, 
+                const Vector& center_Approxbox,
+		const Interval& timeslice,
+                double tsteps,
+		IntervalMatrix& ExpM,
+		IntervalVector& evolCenter,
+		IntervalVector& tauXbox) {
+        /* computation of the Jacobian , and temporal differentiation */
+        const Interval Unt(0.0,1.0);
+        IntervalVector tdiff(dim);
+        IntervalMatrix jac = this->jacobian(approxbox,timeslice,tdiff);
+        Matrix center_jac = jac.mid();
+        jac = jac - center_jac;
+        Vector center_tdiff = tdiff.mid();
+        tdiff = tdiff - center_tdiff;
+
+        // compute cumulated uncertainty on f (parallel linearization) :
+        // f(x,t) in f(xc,tc) + [df/dt(x,t)] (t-tc) + [df/dx(x,t)](x-c)
+        // in mid(f(xc,tc)) + err(f(xc,tc))
+        //             + mid(df/dt) (t-tc) + mid(df/dx)(x-c)
+        //             + |err(df/dt)| |rad(time)| + |err(df/dx)| |rad(x)|
+        IntervalVector uncert = jac*(approxbox-center_Approxbox);
+        uncert += timeslice.rad() * tdiff;
+        IntervalVector fun_eval = 
+		this->teval_vector(timeslice.mid(),center_Approxbox);
+        ibex::Vector fun_evalc = fun_eval.mid();
+        uncert += (fun_eval-fun_evalc);
+  
+        // most probably useless
+#if 0
+        ibex::IntervalVector u2 = fun->eval_vector(approxbox)-
+  	   fun->eval_vector(center_Approxbox) -
+	       (approxbox-center_Approxbox)*center_jac;
+        uncert &= u2;
+#endif
+  
+        IntervalVector tauCent(dim);
+        IntervalMatrix tauprodExpM(dim,dim);
+        this->compute_next_step(fun_evalc,center_tdiff,center_jac,
+		uncert, tsteps, evolCenter, ExpM, tauCent, tauprodExpM);
+
+	// imprecise computation of intermediate box from
+        // Xbox (instead of Cent + prodExpM X0c)
+	// probably not useful to do better (?)
+        tauXbox = Xbox + 
+		Unt*(tauprodExpM*(Xbox-center_Approxbox) + tauCent);
+        tauXbox &= constraint;
+  }
+
   
   ///// forward differential inclusion X' = f(X,t)
   TubeVector DiffInclusion::fwd_inclusion(const IntervalVector& frame, 
                                const IntervalVector& X0) {
        assert(fun != NULL);
        assert(X0.dim()==dim);
-       const double tsteps = time.diam()/nbsteps;
+       const double cst_tsteps = time.diam()/nbsteps;
+       double tsteps = cst_tsteps;
+       double inflation_f = default_inflation_factor;
   
        IntervalVector Cent(X0.mid());
-       const Interval Unt(0.0,1.0);
        const IntervalVector X0c = X0-Cent; // centered "initial" place
        const Matrix Id = Matrix::eye(dim);
        IntervalMatrix prodExpM(Id); // products of exponential
@@ -238,58 +310,50 @@ namespace diffincl {
   
        Result.sample(time.lb(),X0);
        double oldtime = time.lb();
-       for (int currentstep=1;currentstep<=nbsteps;currentstep++) {
-           double currenttime=time.lb()+currentstep*tsteps;
+       while (oldtime<time.ub()) {
+           double currenttime=oldtime+tsteps;
            if (currenttime>time.ub()) currenttime=time.ub();
            Interval timeslice(oldtime,currenttime);
            IntervalVector approxbox = 
-  		this->extend_box_basic(frame,Xbox,timeslice);
+  		this->extend_box_basic(frame,Xbox,timeslice,inflation_f,
+				tsteps>=cst_tsteps/4.0);
            Vector center_Approxbox = approxbox.mid();
-           IntervalVector tdiff(dim);
-           IntervalMatrix jac = this->jacobian(approxbox,timeslice,tdiff);
-           Matrix center_jac = jac.mid();
-           jac = jac - center_jac;
-           Vector center_tdiff = tdiff.mid();
-           tdiff = tdiff - center_tdiff;
-
-           // compute uncert on f : 
-           // f(x,t) in f(xc,tc) + [df/dt(x,t)] (t-tc) + [df/dx(x,t)](x-c)
-           // in mid(f(xc,tc)) + err(f(xc,tc))
-           //             + mid(df/dt) (t-tc) + mid(df/dx)(x-c)
-           //             + |err(df/dt)| |rad(time)| + |err(df/dx)| |rad(x)|
-//           std::cout << "tdiff : " << center_tdiff << "\ntdiffuncert : " << tdiff << "\n";
-           IntervalVector uncert = jac*(approxbox-center_Approxbox);
-           uncert += timeslice.rad() * tdiff;
-           IntervalVector fun_eval = 
-			this->teval_vector(timeslice.mid(),center_Approxbox);
-           ibex::Vector fun_evalc = fun_eval.mid();
-           uncert += (fun_eval-fun_evalc);
-  
-           // FIXME : most probably useless
-#if 0
-           ibex::IntervalVector u2 = fun->eval_vector(approxbox)-
-		fun->eval_vector(center_Approxbox) -
-		(approxbox-center_Approxbox)*center_jac;
-           uncert &= u2;
-#endif
-  
-           IntervalVector tauCent(dim);
+           IntervalMatrix ExpM(dim,dim);
+           IntervalVector evolCenter(dim);
            IntervalVector tauXbox(dim);
-           IntervalMatrix tauprodExpM(dim,dim);
-           this->compute_next_step(center_Approxbox,
-		fun_evalc,center_tdiff,center_jac,
-		uncert, tsteps, Cent,prodExpM, tauCent, tauprodExpM);
-           tauXbox = Xbox + 
-		Unt*(tauprodExpM*(Xbox-center_Approxbox) + tauCent); // FIXME
-           Xbox = Cent + prodExpM * X0c ;
+
+
+	   // computation of the step 
+           this->fwd_step(frame,Xbox,approxbox,center_Approxbox,
+		timeslice, tsteps, ExpM, evolCenter, tauXbox);
+			
+
            if (!tauXbox.is_subset(approxbox)) {
-              std::cerr << "Erreur Xbox//tauXbox//approxbox !!!\n" <<
-                  Xbox << "  " << tauXbox << "  " << approxbox << "\n\n";
+              if (debug_level>=3)
+                std::cerr << "Erreur tauXbox//approxbox !!!\n" 
+                    << tauXbox << "  " << approxbox << " tsteps " <<
+                       tsteps <<  " inf_f " << inflation_f << "\n\n";
+	      // trying a smaller time slice
+	      tsteps/=2.0;
+              inflation_f *= 1.3;
+              continue;
            } 
-           std::cerr << "time : " << currenttime << " box : " << Xbox << "\n";
+
+	   // domain at the new time
+	   Cent = center_Approxbox + ExpM*(Cent-center_Approxbox) + evolCenter;
+	   prodExpM = ExpM * prodExpM;
+           oldtime=currenttime;
+	   // Computation of Xbox (for the tube)
+           Xbox = Cent + prodExpM * X0c ;
+	   Xbox &= frame;
+           if (debug_level>=1) 
+              std::cerr << "time : " << currenttime 
+		        << " box : " << Xbox << "\n";
            Result.set(tauXbox,timeslice);
            Result.set(Xbox,currenttime);
-           oldtime=currenttime;
+           // resetting the time step 
+	   tsteps=cst_tsteps;
+	   inflation_f=default_inflation_factor;
        }
        return Result;
   }
--- diffincl.h
+++ diffincl.h
@@ -16,38 +16,73 @@ namespace diffincl {
 class DiffInclusion 
 {
    public :
+      /**
+       * constructor for non-autonomous function
+       */
       DiffInclusion(unsigned int dim, const TFunction* tfun,
 		    Interval& time, unsigned int nbsteps);
 
+      /**
+       * constructor for autonomous function
+       */
       DiffInclusion(unsigned int dim, const Function* fun,
 		    Interval& time, unsigned int nbsteps);
 
-      // reading from input 
+      /** 
+       * construction from an input 
+       * syntax :
+       * dim:=<dimension>
+       * timedependent:=<non-autonomous?>
+       * time:=[<start>,<end>]
+       * function:= (<v1>,<v2>...) : <function>
+       * nbsteps:= <minsteps>
+       */
       DiffInclusion(std::istream& input);
 
+      /**
+       * number of variables (without time)
+       */
       int get_dim() const;
      
-      // evaluate function on box + time (time dependant or not)
+      /** evaluate function on box + time interval
+       */
       const IntervalVector teval_vector(const Interval &tim,
                  const IntervalVector& cdom);
+      /** evaluate function on box + time
+       */
       const IntervalVector teval_vector(double tim,
                  const IntervalVector& cdom);
+      /** evaluate function on point + time
+       */
       const IntervalVector teval_vector(double tim,
                  const Vector& cdom);
 
-      // compute jacobian ; in case of time dependency, tvec is the first 
-      // column of the jacobian
+      /** compute the jacobian ; 
+       * in case of time dependency, tvec is the first 
+       * column of the jacobian	(dfun / dt), the return is the rest
+       */
       IntervalMatrix jacobian(const IntervalVector& codom,
                  const Interval& tdom,
                  IntervalVector& tvec);
 
-      ///// basic enclosing of a time frame
+      /** basic estimation of the set of states in the following time interval
+       * two steps : 
+       *  1) compute f1=fun(Xbox), f2=fun(Xbox+0.5*dt*f1), 
+       *             f3=fun(Xbox+dt*(f1|f2)) and B = Xbox+[0,dt]*(f1|f2|f3)
+       *  2) ``inflate'' B, intersect with frame, recompute
+       *                 B = Xbox+[0,dt]*fun(B) and intersect with frame
+       * @param frame : constraint on the set
+       * @param Xbox : initial set of tates
+       * @param tim : time interval
+       * TODO : how to customize the policy?
+       */
       IntervalVector extend_box_basic
 		(const IntervalVector& frame, const IntervalVector& Xbox,
-                 const Interval& tim);
+                 const Interval& tim, 
+		 double inflation_factor = default_inflation_factor,
+		 bool last_contraction=true);
 
 	// compute next step, from linear approximation and uncertainty
-        // centzone : center of approxbox
 	// fcent = f(center of the zone, half time)
 	// tVect = mid df/dt
 	// jac : mid jacobian
@@ -55,16 +90,26 @@ class DiffInclusion
 	// uncert : U value (uncertity on f)
 	// double tsteps : time step
 	// f(x,t) \in fcent + (t-tmid)*tvect + jac*(x-x0) + uncert
-      void compute_next_step(const Vector& centzone,
-			 const Vector& fcent,
+      void compute_next_step(const Vector& fcent,
                          const Vector& tVect,
                          const Matrix& jac,
                          const IntervalVector& uncert,
                          double tsteps,
-                         IntervalVector& Cent,
-                         IntervalMatrix& prodExpM,
+                         IntervalVector& evolCenter,
+                         IntervalMatrix& ExpM,
                          IntervalVector& tauCent,
                          IntervalMatrix& tauProdExpM);
+
+
+      void fwd_step(const IntervalVector& constraint,
+                const IntervalVector& Xbox,
+                const IntervalVector& approxbox,
+                const Vector& center_Approxbox,
+                const Interval& timeslice,
+                double tsteps,
+                IntervalMatrix& ExpM,
+                IntervalVector& evolCenter,
+                IntervalVector& tauXbox);
 
 
       ///// differential inclusion        X' = f(X)
@@ -72,6 +117,8 @@ class DiffInclusion
       // X0 : initial position
       TubeVector fwd_inclusion(const IntervalVector& frame, 
 	      	               const IntervalVector& X0);
+
+
 
     private:
 
@@ -82,6 +129,12 @@ class DiffInclusion
           // if time_dependant is true, then the first variable is time
       Interval time;
       unsigned int nbsteps;
+
+      /** customizable fields */
+      int debug_level = 3;
+
+      /** inflation factor in extend_box_basic */
+      constexpr static double default_inflation_factor = 1.2; 
 
       IntervalMatrix* proj=NULL;
 };
